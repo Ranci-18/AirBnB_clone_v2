f2bbb13190f144fb876e24a2c1eb93e2c0084d05
James Yuri <yurijamesmail@gmail.com>
2023-06-18 21:20:29 +0300

    Mysql testdb setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,8 @@
--- script prepares a MYSQL server for the project
--- it first creates database hbnb_dev_db without fail
--- then creates user hbnb_dev@localhost without fail
--- grants all permissions on hbnb_dev_db
--- grants select permissions on performance_schema
+-- Creates hbnb_test_db database if it does not exists
+-- Creates user hbnb_test@localhost if it does not exist
+-- Grants all priviledges to the user on the hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `perfomance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
