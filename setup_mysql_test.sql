d74a0a1501a05a024a5eca6cdecbc91ac93d2ae1
James Yuri <yurijamesmail@gmail.com>
2023-06-19 11:17:06 +0300

    mysql test fix attempt

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,6 +3,5 @@
 -- Grants all priviledges to the user on the hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
