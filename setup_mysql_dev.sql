86e3d5334b567fb6532acd978ad2bd14a3055cea
Frank <wanyoike39@gmail.com>
2023-06-19 09:44:14 +0300

    task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- script prepares a MYSQL server for the project
+-- it first creates database hbnb_dev_db without fail
+-- then creates user hbnb_dev@localhost without fail
+-- grants all permissions on hbnb_dev_db
+-- grants select permissions on performance_schema
+CREATE IF NOT EXISTS DATABASE hbnb_dev_db;
+CREATE IF NOT EXISTS USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
