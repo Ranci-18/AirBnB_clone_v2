b08187d161ff41bf76c873b7b4ab9aee19c41436
Frank <wanyoike39@gmail.com>
2023-06-19 10:05:29 +0300

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,8 +3,8 @@
 -- then creates user hbnb_dev@localhost without fail
 -- grants all permissions on hbnb_dev_db
 -- grants select permissions on performance_schema
-CREATE IF NOT EXISTS DATABASE hbnb_dev_db;
-CREATE IF NOT EXISTS USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
