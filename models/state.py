92abb6134855f2e3e12f5990b7bc5d406d024aed
Frank <wanyoike39@gmail.com>
2023-06-21 01:10:33 +0300

    task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    cities = relationship('City', cascade='all, delete', backref='state')
+
+    def cities(self):
+        """getter method returns list of city instances"""
+        from models import storage
+        city_list = []
+        for city in storage.all('City').values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
