627080063b166eb3c2698a27304f3be223faef62
Frank <wanyoike39@gmail.com>
2023-06-21 01:47:09 +0300

    task 6 fifth fix attempt

--- models/state.py
+++ models/state.py
@@ -3,21 +3,30 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import environ
 
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
 
-    cities = relationship('City', cascade='all, delete', backref='state')
+        cities = relationship('City', cascade='all, delete-orphan',
+                               backref='state', passive_deletes=True)
 
-    def cities(self):
-        """getter method returns list of city instances"""
-        from models import storage
-        city_list = []
-        for city in storage.all('City').values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        def cities(self):
+            """getter method returns list of city instances"""
+            from models import storage
+            city_list = []
+            for city in storage.all('City').values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
