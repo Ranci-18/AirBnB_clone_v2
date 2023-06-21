84c2e16133be187b53a9c00c2d6e6739f1cec8b3
Frank <wanyoike39@gmail.com>
2023-06-21 21:30:38 +0300

    all tasks fix attempts

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    name = Column(String(128), nullable=False)
+
+    def __init__ = 'amenities'
+    super().__init__(*args, **kwargs)
