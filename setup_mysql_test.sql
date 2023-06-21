094eefd38271cc605edfe55808ab6eed86cf55b9
James Yuri <yurijamesmail@gmail.com>
2023-06-21 22:54:24 +0300

    fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,8 @@
 -- Creates hbnb_test_db database if it does not exists
 -- Creates user hbnb_test@localhost if it does not exist
 -- Grants all priviledges to the user on the hbnb_test_db
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
