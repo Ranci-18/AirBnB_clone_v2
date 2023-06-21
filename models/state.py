0e14dc11209a84f014e4deb7d6ac2bf7aff2022a
James Yuri <yurijamesmail@gmail.com>
2023-06-21 22:30:27 +0300

    console.py fix

--- models/state.py
+++ models/state.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class State(BaseModel):
+    """ State class """
+    name = ""
