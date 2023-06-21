8660be8b2bc0b7969e41f72c2d2a5eb20a297b5e
James Yuri <yurijamesmail@gmail.com>
2023-06-21 22:15:35 +0300

    rollback everything

--- models/state.py
+++ models/state.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import environ
+"""This module defines a class User"""
+from models.base_model import BaseModel
 
 
-
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-
-        cities = relationship('City', cascade='all, delete-orphan',
-                               backref='state', passive_deletes=True)
-
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    if environ.get('HBNB_TYPE_STORAGE') != 'db':
-        def cities(self):
-            """getter method returns list of city instances"""
-            from models import storage
-            city_list = []
-            for city in storage.all('City').values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+class User(BaseModel):
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
--- models/user.py
+++ models/user.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """
-    This is the class for user
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete-orphan',
-                          backref="user", passive_deletes=True)
-    reviews = relationship("Review", cascade='all, delete-orphan',
-                           backref="user", passive_deletes=True)
+class User(BaseModel):
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
