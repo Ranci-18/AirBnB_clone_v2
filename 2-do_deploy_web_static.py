fd0018394434086827550eda27e01ecb72d7aaed
Frank <wanyoike39@gmail.com>
2023-07-06 13:14:20 +0300

    task 2 fix - deploying archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
-""" Fabric script (based on the file 1-pack_web_static.py)
+"""Fabric script that generates a .tgz
+archive from the contents of the web_static
+folder of your AirBnB Clone repo,
+using the function do_pack and Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers,
 using the function do_deploy"""
 from fabric.api import *
 import os
+from fabric.api import local
+from datetime import datetime
 
+
+def do_pack():
+    """function to generate .tgx archive"""
+    try:
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive = "versions/web_static_{:s}.tgz".format(date)
+        local("tar -cvzf {:s} web_static".format(archive))
+        return archive
+    except:
+        return None
 
 env.hosts = ['54.236.25.73 web-01', '52.91.178.190 web-02']
 env.key_filename = "~/.ssh/school"
