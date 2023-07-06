7f9b8f638fb13f30c31c1787d7680374e7b1e83b
Frank <wanyoike39@gmail.com>
2023-07-06 13:07:04 +0300

    task 2 - deploying archive

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
+using the function do_deploy"""
+from fabric.api import *
+import os
+
+
+env.hosts = ['54.236.25.73 web-01', '52.91.178.190 web-02']
+env.key_filename = "~/.ssh/school"
+env.user = "ubuntu"
+
+
+@task
+def do_deploy(archive_path):
+    """function to deploy archive to web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    archive_filename = os.path.basename(archive_path)
+    release_path = "/data/web_static/releases/" + archive_filename[:-4]
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {:s}".format(release_path))
+        run("tar -xzf /tmp/{:s} -C {:s}".format(archive_filename, release_path))
+        run("rm /tmp/{:s}".format(archive_filename))
+        run("mv {:s}/web_static/* {:s}/".format(release_path, release_path))
+        run("rm -rf {:s}/web_static".format(release_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {:s} /data/web_static/current".format(release_path))
+        print("New version deployed!")
+        return True
+    except:
+        return False
